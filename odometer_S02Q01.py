c9309d30d6daf682709243f8e3ebb6525da634f5
Ramyacherukuru <130358891+Ramyacherukuru@users.noreply.github.com>
2023-04-12 09:07:47 +0530

    code for calculating mileage of the car

---
+++ odometer_S02Q01.py
@@ -0,0 +1,6 @@
+Starting_values = float(input("Enter the starting_value:"))
+Ending_values = float(input("Enter the Endingvalue:"))
+distance_travelled = Ending_values - Starting_values
+fuel_consumed = float(input("Enter the value of fuel_consumed:"))
+car_mileage = distance_travelled/fuel_consumed
+print("car_mileage:", car_mileage)
