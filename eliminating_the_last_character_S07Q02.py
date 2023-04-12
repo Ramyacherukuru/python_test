5f4c9ed6d29ab13432fdce66bd7b3fdd6ceb2e88
Ramyacherukuru <130358891+Ramyacherukuru@users.noreply.github.com>
2023-04-12 14:42:05 +0530

    Eliminating the last character of a strings

---
+++ eliminating_the_last_character_S07Q02.py
@@ -0,0 +1,12 @@
+Name = str(input("Enter the Name:"))
+Surname = str(input("Enter the Surname:"))
+print(Name, Surname)
+for i in range(4):
+    Name = Name[:-1]
+    Surname = Surname[:-1]
+    print(Name , Surname)
+
+
+
+            
+                 
