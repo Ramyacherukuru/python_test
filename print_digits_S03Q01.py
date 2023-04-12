a31d8a2dbfdd5aced465d4b3fde02b9343b9a1bf
Ramyacherukuru <130358891+Ramyacherukuru@users.noreply.github.com>
2023-04-12 09:13:14 +0530

    code for print 2 digits , 3 digits

---
+++ print_digits_S03Q01.py
@@ -0,0 +1,8 @@
+n = int(input("Enter the number:"))
+if n >= 10 and n <= 99:
+    print("It is a two digit number")
+elif n >= 100 and n <= 999:
+    print("It is a three digit number")
+else:
+    print(n)
+            
