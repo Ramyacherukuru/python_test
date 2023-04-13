59a80b3535cf45c937c61d7f6b88ab3c3faea36f
Ramyacherukuru <130358891+Ramyacherukuru@users.noreply.github.com>
2023-04-13 19:30:50 +0530

    code to print last 2,3 digits of number

---
+++ print_last_2,3_digits_S08Q02.py
@@ -0,0 +1,12 @@
+n = int(input("Enter the number:"))
+if n < 10:
+    n = n + 7
+    print(n%10)
+elif n >= 10 and n <= 99:
+    n = n ** 5
+    print(n%100)
+elif n >= 100 and n <= 999:
+    n1 = int(input("Enter the another number:"))
+    n = n + n1
+    print(n%1000)
+    
