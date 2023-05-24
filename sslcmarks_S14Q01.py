5161df15c0dbbec9d96e1e304ab67742e095994f
Ramyacherukuru <130358891+Ramyacherukuru@users.noreply.github.com>
2023-05-24 16:16:00 +0530

    code to print the names and marks
    of all students who have scored more than 90% marks, in ascending order of their marks.

---
+++ sslcmarks_S14Q01.py
@@ -0,0 +1,56 @@
+students = []
+while True:
+    student = input("Enter the name and marks of the student:")
+    if student =='':
+        break
+    name,marks = student.strip().split(',')
+    students.append((name,int(marks)))
+
+s = sorted(students,  key = lambda  x:x[1])
+print("stdedgvhhgf more than 90:")
+
+for student in s:
+    if student[1] > 90:
+        print(student[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
